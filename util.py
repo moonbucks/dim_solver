cf57c332d6fd9f41b6c217a24d67d43bf80ec2f6
Yeonju Ro <yro@meta.com>
2023-07-18 23:29:01 +0000

    added argument parser

--- util.py
+++ util.py
@@ -1,4 +1,23 @@
+import argparse
+import os
+
+def get_args():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--num-gpu', type=int, default=4)
+
+  # system
+  parser.add_argument('--network-bandwidth', type=float, default=300, description='network bandwith in GB/s') # default=nvlink
+  parser.add_argument('--compute-performance', type=float, default=19.5, description='single-precision floating point performance in TFLOPS') # default=A100 
+
+  # model
+  parser.add_argument('--num-layers', type=int, default=12)
+  parser.add_argument('--batch-size', type=int, default=12)
+
+  args = parser.parse_args()
+  return args
+
 def get_all_possible_dims (num_gpu):
+  # TODO support non power-of-two numbers
   lst = [[num_gpu,1]]
   tp, pp = 1, num_gpu
   while pp>1:
@@ -18,3 +37,5 @@ if __name__ == '__main__':
   get_all_possible_dims(512)
   get_all_possible_dims(1024)
   get_all_possible_dims(2048)
+  get_all_possible_dims(1008)
+  get_all_possible_dims(1052)
