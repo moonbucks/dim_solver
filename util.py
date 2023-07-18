83fb91152449cd9338a5e88b2d773268cb9772cf
Yeonju Ro <yro@meta.com>
2023-07-18 21:04:57 +0000

    return all possible 2d mesh

--- util.py
+++ util.py
@@ -1,7 +1,21 @@
 def possible_dim (num_gpu):
 
-  lst = []
+  lst = [[num_gpu,1]]
   tp, pp = 1, num_gpu
-  while
+  while pp>1:
+    pp /=2
+    tp *=2
+    lst.append([int(pp),int(tp)])
 
-return lst
+  print(f'num_gpu:{num_gpu}, len(lst)={len(lst)}, lst={lst}')
+  return lst
+
+
+if __name__ == '__main__':
+  possible_dim(16)
+  possible_dim(32)
+  possible_dim(64)
+  possible_dim(256)
+  possible_dim(512)
+  possible_dim(1024)
+  possible_dim(2048)
