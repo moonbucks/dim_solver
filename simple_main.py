c82e47dad4a0d9ccacb6da1f2bcd99c46114894a
Yeonju Ro <yro@meta.com>
2023-07-27 09:55:40 -1000

    does this work?

--- simple_main.py
+++ simple_main.py
@@ -1,60 +1,66 @@
 from ortools.linear_solver import pywraplp
+from util import get_all_possible_dims, get_args, set_model_args
+from cost_linear import pipeline_cost
 
-solver = pywraplp.Solver('Minimize Cost', pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+def solve(args, n_stage, tp):
+  solver = pywraplp.Solver('Minimize Cost', pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
 
-# gpt2 config
-world_size = 4
-batch_size = 12 
-num_layers = 12
+  ## variables 
+  #n_chunk = solver.IntVar(1, solver.infinity(), 'Number of chunks')
+  size_microbatch = solver.IntVar(1, solver.infinity(), 'Size of microbatch')
+  i_stage = solver.IntVar(1, solver.infinity(), 'Internal pipeline depth')
 
-n_stage = solver.IntVar(1, solver.infinity(), 'Pipeline depth')
-n_chunk = solver.IntVar(1, solver.infinity(), 'Number of chunks')
-#i_stage = solver.IntVar(1, solver.infinity(), 'Internal pipeline depth')
-p_stage = solver.IntVar(1, solver.infinity(), 'Entire Pipeline depth, n_stage * i_stage')
+  ## constraints
+  solver.Add(size_microbatch <= args.batch_size)
+  solver.Add(i_stage <= 2 * args.num_layers / n_stage)
+  #solver.Add(n_chunk >= 4 * n_stage) # recommended since it is a 'recommended' thing, it is actually not a 'constraint'. this can be relaxed  
+  solver.Add(size_microbatch >= i_stage - 1) # preferred to hide all internal ARs actually this is more likely 
 
+  ## objective 
 
-## constraints
-#solver.Add(n_stage <= world_size)
-#solver.Add(n_chunk <= batch_size)
-#solver.Add(i_stage <= 2 * num_layers / n_stage) # Warning multiplication
-#solver.Add(n_chunk >= 4 * n_stage) # recommended 
-#solver.Add(n_chunk >= i_stage - 1) # not a "requirement"
+  #toy
+  #cost = toy_cost(n_chunk)
 
-# new constraints
-solver.Add(n_stage <= world_size)
-solver.Add(n_chunk <= batch_size)
-solver.Add(p_stage <= 2 * num_layers)
-solver.Add(n_chunk >= 4 * n_stage) # recommended 
-solver.Add(n_stage / 2 >= 0)
+  #real
+  cost = pipeline_cost(args, n_stage, size_microbatch, i_stage)
 
+  solver.Minimize(cost)
 
-# cost function
+  status = solver.Solve()
 
-#toy
-cost = n_chunk 
+  ## results 
+  if status == pywraplp.Solver.OPTIMAL: # or status == pywraplp.Solver.FEASIBLE:
+    print(f'================= Solution for {[n_stage, tp]} =================')
+    print(f'Solved in {solver.wall_time():.2f} milliseconds in {solver.iterations()} iterations')
+    print(f' - Optimal Cost = {solver.Objective().Value()} ')
+    print(f' - Size of Microbatch = {size_microbatch.solution_value()}')
+    print(f' - Internal Pipeine Depth = {i_stage.solution_value()}')
+    print()
 
+    return solver.Objective().Value(), size_microbatch.solution_value(), i_stage.solution_value()
 
-#real
-#comm_cost = 
-#K = comm_cost + comp_cost # unit cost 
+  else:
+    print(f'================= Solution for {[n_stage, tp]} =================')
+    print('The solver could not find an optimal solution.')
+    print()
 
+    return float('inf'), -1, -1
 
-solver.Maximize(cost)
+if __name__ == '__main__':
+  args = get_args()
+  set_model_args(args, 'gpt3-175b')
+  lst = get_all_possible_dims(args.num_gpu)
+  best_cost = float('inf')
+  best_dim = [1,1]
+  for dim in lst:
+    if dim[0] > args.num_layers: # assert pp_size <= world_size
+      continue
 
-status = solver.Solve()
+    cost, size_microbatch, i_stage = solve(args, dim[0], dim[1])
+    if cost < best_cost:
+      best_cost = cost
+      best_dim = dim
+      best_size_microbatch = size_microbatch
+      best_i_stage = i_stage
 
-# If an optimal solution has been found, print results
-if status == pywraplp.Solver.OPTIMAL or status == pywraplp.Solver.FEASIBLE:
-  print('================= Solution =================')
-  print(f'Solved in {solver.wall_time():.2f} milliseconds in {solver.iterations()} iterations')
-  print(f'Optimal value = {solver.Objective().Value()} ')
-  print(f' - Pipeline Depth = {n_stage.solution_value()}')
-  print(f' - Number of Chunks = {n_chunk.solution_value()}')
-  #print(f' - Internal Pipeine Depth = {i_stage.solution_value()}')
-  print(f' - Total Pipeine Depth = {p_stage.solution_value()}')
-else:
-  print('The solver could not find an optimal solution.')
-
-
-# Reference
-# https://towardsdatascience.com/introduction-to-linear-programming-in-python-9261e7eb44b
+  print(best_dim, best_cost, 'size_microbatch:', size_microbatch, 'internal stage:', best_i_stage)
